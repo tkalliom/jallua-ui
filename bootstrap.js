c0ea57ad5f1f79422a02635bcea67cfbe45a6867
Timo Kalliomäki <timoka10@gmail.com>
2016-12-02 03:35:47 +0200

    Fixed compilation writing in node_modules

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowser } from '@angular/platform-browser';
 
-import { AppModuleNgFactory } from './scripts/app/app.module.ngfactory';
+import { AppModuleNgFactory } from './scripts/factories/app/app.module.ngfactory';
 
 enableProdMode();
 const platform = platformBrowser();
