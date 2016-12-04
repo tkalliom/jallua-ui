548889bdab5fecfe4c9e2b67ed8862a50e23bad6
Timo Kalliomäki <timoka10@gmail.com>
2016-12-04 16:08:43 +0200

    Show weather condition data

--- app/store-detail.component.ts
+++ app/store-detail.component.ts
@@ -11,6 +11,9 @@ import { Store } from "./store";
           <dt>id</dt><dd>{{store.storeNumber}}</dd>
           <dt>address</dt><dd>{{store.streetAddress}}</dd>
           <dt>post office</dt><dd>{{store.postOffice}}</dd>
+          <dt>condition</dt><dd>{{store.condition.text}}</dd>
+          <dt>temp</dt><dd>{{store.condition.temp}}</dd>
+          <dt>conditions at</dt><dd>{{store.condition.date}}</dd>
       </dl>
     </div>
   `
--- app/store.ts
+++ app/store.ts
@@ -2,6 +2,12 @@ export class Store {
   storeNumber: string;
   streetAddress: string;
   postOffice: string;
+  condition: {
+    code: number;
+    text: string;
+    temp: number;
+    date: string;
+  };
 
   get displayName(): string {
     return `${this.streetAddress}, ${this.postOffice}`;
