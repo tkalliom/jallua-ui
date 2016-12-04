98dce2fa53cf9f5220848a725988f2a5a4545c4c
Timo Kalliomäki <timoka10@gmail.com>
2016-12-04 22:20:43 +0200

    Do not display weather data fields if none is present

--- app/store-detail.component.ts
+++ app/store-detail.component.ts
@@ -11,9 +11,9 @@ import { Store } from "./store";
           <dt>id</dt><dd>{{store.storeNumber}}</dd>
           <dt>address</dt><dd>{{store.streetAddress}}</dd>
           <dt>post office</dt><dd>{{store.postOffice}}</dd>
-          <dt>condition</dt><dd>{{store.condition.text}}</dd>
-          <dt>temp</dt><dd>{{store.condition.temp}}</dd>
-          <dt>conditions at</dt><dd>{{store.condition.date}}</dd>
+          <dt *ngIf="store.condition.text">condition</dt><dd>{{store.condition.text}}</dd>
+          <dt *ngIf="store.condition.temp">temp</dt><dd>{{store.condition.temp}}</dd>
+          <dt *ngIf="store.condition.date">conditions at</dt><dd>{{store.condition.date}}</dd>
       </dl>
     </div>
   `
